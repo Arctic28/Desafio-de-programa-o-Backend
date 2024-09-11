330918e79d0611cb33ef3369151f77a13f896dad
ARTHUR VICTOR <ARTHUR9402@IOT>
2024-09-11 11:23:11 -0300

    outro commit

--- index.js
+++ index.js
@@ -9,13 +9,34 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send(garantia);
+    console.log(garantia);
+    
 });
 
-app.get('.garantia:id', (req, res) => {
+let clients = [];
+
+app.post('/cliente', (req, res) => {
+    const { name } = req.body;
+    const id = clients.length + 1;
+    clients.push({ id, name });
+    res.send('Client Added')
+});
+
+app.put('cliente/:id', (req, res) => {
     const { id } = req.params;
-    const garantia = garantia.find((item) => item.id === Number(id));
-    if (!garantia) return res.send('Garantia not Found');
-    res.send(garantia)
+    const { name } = req.body;
+    const client = clients.find((value) => value.id === Number(id));
+    if (!client) return res.send('Client Not Found');
+    client.name = name
+    req.send('Client updated');
+});
+
+app.get('/garantia/:id', (req, res) => {
+    const id = req.params.id;
+    const itemgarantia = garantia.find((item) => item.id === Number(id));
+    if (!itemgarantia) return res.send('Garantia not Found');
+    res.send(itemgarantia)
+
 });
 
 app.listen(3000, () => {
